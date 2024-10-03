50274d5524b345f019efa156e9a9f200edc669f9
hamnerben <hamnerben@gmail.com>
2024-10-02 20:28:36 -0600

    trying to get the donut into obsidian

--- src/view.tsx
+++ src/view.tsx
@@ -3,7 +3,7 @@ import {createRoot, Root} from "react-dom/client";
 import * as exp from "constants";
 import { ItemView, TFile, WorkspaceLeaf, debounce } from "obsidian";
 import { getAllDailyNotes, getDateUID, getDateFromFile } from "obsidian-daily-notes-interface";
-import YearCircle from "./components/yearCircle";
+import YearCircle from "./components/YearCircle";
 
 export const DECADE_VIEW = "decade-view";
 
